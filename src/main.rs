2407cf444d09673e697e117c31ea4f747db956e2
Jim Eckerlein <jim.eckerlein@icloud.com>
2025-03-11 00:12:12 +0100

    update to bevy 0.15

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() -> impl Termination {
         })
         .add_plugins(DefaultPlugins)
         .add_plugins(camera::CameraPlugin)
-        .insert_resource(Msaa::Off)
         .add_systems(Startup, startup)
         .add_systems(Update, update)
         .run()
@@ -27,66 +26,53 @@ fn startup(
     // Spawn a cube
     commands.spawn((
         Marker(0.0),
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(Cuboid {
-                half_size: Vec3::splat(0.5),
-            })),
-            material: materials
-                .add(StandardMaterial {
-                    base_color: Color::srgb(0.5, 0.5, 0.5),
-                    ..Default::default()
-                })
-                .clone(),
-            transform: Transform::from_xyz(1.0, 0.5, 0.0),
+        Mesh3d(meshes.add(Mesh::from(Cuboid {
+            half_size: Vec3::splat(0.5),
+        }))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.5, 0.5, 0.5),
             ..Default::default()
-        },
+        })),
+        Transform::from_xyz(1.0, 0.5, 0.0),
     ));
     commands.spawn((
         Marker(1.5),
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(Cuboid {
-                half_size: Vec3::splat(0.5),
-            })),
-            material: materials
-                .add(StandardMaterial {
-                    base_color: Color::srgb(0.5, 0.5, 0.5),
-                    ..Default::default()
-                })
-                .clone(),
-            transform: Transform::from_xyz(0.0, 0.5, 1.0),
+        Mesh3d(meshes.add(Mesh::from(Cuboid {
+            half_size: Vec3::splat(0.5),
+        }))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.5, 0.5, 0.5),
             ..Default::default()
-        },
+        })),
+        Transform::from_xyz(0.0, 0.5, 1.0),
     ));
 
     // Spawn a plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(PlaneMeshBuilder {
+    commands.spawn((
+        Mesh3d(meshes.add(Mesh::from(PlaneMeshBuilder {
             plane: Plane3d {
                 normal: Dir3::Y,
                 half_size: Vec2::splat(4.0),
             },
             subdivisions: 0,
+        }))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.5, 0.5, 0.5),
+            ..Default::default()
         })),
-        material: materials
-            .add(StandardMaterial {
-                base_color: Color::srgb(0.5, 0.5, 0.5),
-                ..Default::default()
-            })
-            .clone(),
-        ..Default::default()
-    });
+    ));
 }
 
 fn update(
     time: Res<Time>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    query: Query<(&Handle<StandardMaterial>, &Marker)>,
+    query: Query<(&MeshMaterial3d<StandardMaterial>, &Marker)>,
 ) {
     for (material, &Marker(offset)) in query.iter() {
         materials.get_mut(material).unwrap().base_color = Color::srgb(
-            ((offset + time.elapsed_seconds()).sin() * 0.5 + 0.5) as f32,
-            ((offset + time.elapsed_seconds() + TAU / 3.0).sin() * 0.5 + 0.5) as f32,
-            ((offset + time.elapsed_seconds() + 2.0 * TAU / 3.0).sin() * 0.5 + 0.5) as f32,
+            ((offset + time.elapsed_secs()).sin() * 0.5 + 0.5) as f32,
+            ((offset + time.elapsed_secs() + TAU / 3.0).sin() * 0.5 + 0.5) as f32,
+            ((offset + time.elapsed_secs() + 2.0 * TAU / 3.0).sin() * 0.5 + 0.5) as f32,
         );
     }
 }
@@ -95,11 +81,12 @@ mod camera {
     use std::f32::consts::TAU;
 
     use bevy::{
+        core_pipeline::experimental::taa::TemporalAntiAliasing,
         input::{
             gestures::PinchGesture,
             mouse::{MouseScrollUnit, MouseWheel},
         },
-        pbr::{CascadeShadowConfigBuilder, ScreenSpaceAmbientOcclusionBundle},
+        pbr::{CascadeShadowConfig, CascadeShadowConfigBuilder, ScreenSpaceAmbientOcclusion},
         prelude::*,
     };
 
@@ -129,26 +116,26 @@ mod camera {
                     pitch: f32::to_radians(-45.0),
                     radius: 5.0,
                 },
-                VisibilityBundle::default(),
-                Camera3dBundle::default(),
-                ScreenSpaceAmbientOcclusionBundle::default(),
+                Visibility::default(),
+                Camera3d::default(),
+                ScreenSpaceAmbientOcclusion::default(),
+                TemporalAntiAliasing::default(),
+                Msaa::Off,
             ))
             .with_children(|commands| {
-                commands.spawn((DirectionalLightBundle {
-                    directional_light: DirectionalLight {
+                commands.spawn((
+                    DirectionalLight {
                         shadows_enabled: true,
                         illuminance: 1000.0,
                         ..Default::default()
                     },
-                    cascade_shadow_config: CascadeShadowConfigBuilder {
+                    CascadeShadowConfig::from(CascadeShadowConfigBuilder {
                         maximum_distance: 100.0,
                         first_cascade_far_bound: 10.0,
                         ..Default::default()
-                    }
-                    .into(),
-                    transform: Transform::from_rotation(Quat::from_rotation_y(-TAU / 8.0)),
-                    ..default()
-                },));
+                    }),
+                    Transform::from_rotation(Quat::from_rotation_y(-TAU / 8.0)),
+                ));
             });
     }
 
